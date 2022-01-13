6d6219b6755a9b168ba39c8a8c8a49a6e71856ef
Aleksandr Koloshtivin <aykolosh@mts.ru>
2022-01-13 17:15:01 +0300

    Tests fixed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,68 +1,68 @@
-import { DebugElement } from '@angular/core';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { MockComponent } from 'ng-mocks';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule, By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { MockComponent } from 'ng-mocks';
+
+export function findComponent<T>(
+  fixture: ComponentFixture<T>,
+  selector: string,
+): DebugElement {
+  return fixture.debugElement.query(By.css(selector));
+}
 
 describe('AppComponent', () => {
-  beforeEach(fakeAsync(() => {
-    TestBed.configureTestingModule({
+  let fixture: ComponentFixture<AppComponent>;
+  let appComponent: AppComponent;
+  let counterFixture: ComponentFixture<CounterComponent>;
+  let counterComponent: CounterComponent;
+  let debugCounterElement: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        CounterComponent
+        MockComponent(CounterComponent)
       ],
       imports: [
         BrowserModule,
         FormsModule,
         ReactiveFormsModule
       ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    appComponent = fixture.componentInstance;
+
+    counterFixture = TestBed.createComponent(CounterComponent);
+    debugCounterElement = fixture.debugElement.query(By.directive(CounterComponent));
+    counterComponent = debugCounterElement.componentInstance;
+    counterComponent.value = 0;
+  });
+
+  it('should create the app', fakeAsync(() => expect(appComponent).toBeTruthy()));
+
+  it('should render counter component', fakeAsync(() => expect(findComponent(fixture, 'app-counter')).toBeTruthy()));
+
+  it('should render input element in counterComponent', fakeAsync(() => expect(debugCounterElement).toBeTruthy()));
+
+  it('should call counter component writeValue method when the app formControl value changed', fakeAsync(() => {
+    spyOn(counterComponent, 'writeValue').and.callThrough();
+
+    appComponent.counterCtrl.setValue('11');
+    tick();
+
+    expect(counterComponent.writeValue).toHaveBeenCalled();
   }));
 
-  describe('initialization', () => {
-    let fixture: ComponentFixture<AppComponent>;
-    let appComponent: AppComponent;
-    let counterFixture: ComponentFixture<CounterComponent>;
-    let counterComponent: CounterComponent;
-    let debugInputElement: DebugElement;
-    let inputElement: HTMLInputElement;
+  it('counter component writeValue method should change the app formControl value', fakeAsync(() => {
+    counterComponent.writeValue(11);
+    tick();
 
-    beforeEach(fakeAsync(() => {
-      fixture = TestBed.createComponent(AppComponent);
-      fixture.detectChanges();
-      appComponent = fixture.componentInstance;
-
-      counterFixture = TestBed.createComponent(CounterComponent);
-      counterFixture.detectChanges();
-      counterComponent = counterFixture.componentInstance;
-      debugInputElement = fixture.debugElement.query(By.directive(CounterComponent));
-      inputElement = debugInputElement.nativeElement;
-      spyOn(counterFixture.debugElement.componentInstance, 'writeValue').and.callThrough();
-
-    }));
-
-    it('should create the app', fakeAsync(() => expect(appComponent).toBeTruthy()));
-
-    it('should render counter component', fakeAsync(() => expect(fixture.debugElement.query(By.css('app-counter'))).toBeTruthy()));
-
-    it('should render input element in counterComponent', fakeAsync(() => expect(debugInputElement).toBeTruthy()));
-
-    it('should change counter component input value through the app formControl setting value', fakeAsync(() => {
-
-
-      appComponent.counterCtrl.setValue('11');
-      fixture.detectChanges();
-      counterFixture.detectChanges();
-      console.log(counterFixture.debugElement.nativeElement)
-
-      tick();
-
-      expect(counterComponent.writeValue).toHaveBeenCalled();
-
-    }));
-  })
-
-
+    expect(appComponent.counterCtrl.value).toBe(11);
+  }));
 });
